e9e109c8072773642875af4f53ad4f42be477e65
Antonin <antonin.juquel@outlook.fr>
2022-10-17 18:06:07 +0200

    added audio slider

--- AudioManager.cs
+++ AudioManager.cs
@@ -1,13 +1,22 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
 namespace AudioSystem
 {
     public class AudioManager : AudioController
     {
+        [SerializeField] private AudioMixer mixer;
         public static AudioManager Instance;
 
         protected override void Awake()
         {
             base.Awake();
             Instance = this;
+        }
+
+        public void SetMixerValue(string groupName, float value)
+        {
+            mixer.SetFloat(groupName, Mathf.Log10(value) * 20f);
         }
     }
 }
---
+++ AudioSlider.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace AudioSystem
+{
+    [RequireComponent(typeof(Slider))]
+    public class AudioSlider : MonoBehaviour
+    {
+        [SerializeField] private string groupName;
+        private Slider _slider;
+
+        private void Awake()
+        {
+            _slider = GetComponent<Slider>();
+            _slider.minValue = 0.0001f;
+            _slider.maxValue = 1f;
+            _slider.value = PlayerPrefs.GetFloat(name, 1f);
+        }
+
+        private void Start()
+        {
+            SetVolume(_slider.value);
+        }
+
+        private void OnEnable()
+        {
+            _slider.onValueChanged.AddListener(SetVolume);
+        }
+
+        private void OnDisable()
+        {
+            _slider.onValueChanged.RemoveListener(SetVolume);
+        }
+
+        private void SetVolume(float value)
+        {
+            AudioManager.Instance.SetMixerValue(groupName, value);
+            PlayerPrefs.SetFloat(name, value);
+        }
+    }
+}
